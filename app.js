3bd15545e17b18b07026277edf3b6663937f8ec0
shahazad08 <sk.shahazad@gmail.com>
2021-09-22 11:35:03 +0530

    Added: err callback aded

--- app.js
+++ app.js
@@ -17,16 +17,15 @@ const server=http.createServer((req,res)=> {
             body.push(chunk)
             console.log("********")
         });
-        req.on('end', ()=> {
+        return req.on('end', ()=> {
             const parsedBody=Buffer.concat(body).toString();
             const message=parsedBody.split('=')[1]
-            fs.writeFileSync('message.txt',message);
-           
-        })
-        res.statusCode=302;
-        res.setHeader('Location','/')
-        return res.end();
-        
+            fs.writeFile('message.txt',message, err =>  {
+                res.statusCode=302;
+                res.setHeader('Location','/')
+                return res.end();    
+            });     
+        })        
     }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
