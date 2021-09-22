60ddfdd0300e4b208969d04511df392918862382
shahazad08 <sk.shahazad@gmail.com>
2021-09-22 10:47:08 +0530

    Added: Buffer added with Req & Res

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const server=http.createServer((req,res)=> {
     const url=req.url;
     const method=req.method;
     if(url==='/') {
-        console.log("URL Home", url.req)
         res.write('<html>');
         res.write('<head><title>Enter Message</title></head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
@@ -13,13 +12,22 @@ const server=http.createServer((req,res)=> {
         return res.end();
     }
     if(url==="/message" && method==="POST") {
-        console.log("URL Message", url.req)
-        fs.writeFileSync('message.txt','Dummy');
+        const body=[]
+        req.on('data', (chunk)=> {
+            body.push(chunk)
+            console.log("********")
+        });
+        req.on('end', ()=> {
+            const parsedBody=Buffer.concat(body).toString();
+            const message=parsedBody.split('=')[1]
+            fs.writeFileSync('message.txt',message);
+           
+        })
         res.statusCode=302;
         res.setHeader('Location','/')
         return res.end();
+        
     }
-    console.log("URL Home 11", url.req)
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
